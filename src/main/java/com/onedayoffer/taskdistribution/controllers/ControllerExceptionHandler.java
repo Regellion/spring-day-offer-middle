137090010846f3d5301d171215a0dbe16dcd741d
Artem Martynov <amerta90@mail.ru>
2024-03-23 10:53:27 +0300

    Доработана логика работы методов сервиса и контроллера. Тесты проходят успешно

--- src/main/java/com/onedayoffer/taskdistribution/controllers/ControllerExceptionHandler.java
+++ src/main/java/com/onedayoffer/taskdistribution/controllers/ControllerExceptionHandler.java
@@ -1,11 +1,23 @@
 package com.onedayoffer.taskdistribution.controllers;
 
-import org.springframework.http.HttpHeaders;
+import com.onedayoffer.taskdistribution.DTO.ExceptionDTO;
+import com.onedayoffer.taskdistribution.exceptions.TaskNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-//@ControllerAdvice(annotations= RestController.class)
+@ControllerAdvice(annotations= RestController.class)
 public class ControllerExceptionHandler {
+    @ExceptionHandler
+    public ResponseEntity<ExceptionDTO> handleException(TaskNotFoundException exception) {
+        ExceptionDTO exceptionDTO = new ExceptionDTO(exception.getMessage());
+        return new ResponseEntity<>(exceptionDTO, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<ExceptionDTO> handleException(Exception exception) {
+        ExceptionDTO exceptionDTO = new ExceptionDTO(exception.getMessage());
+        return new ResponseEntity<>(exceptionDTO, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 
 }
--- src/main/java/com/onedayoffer/taskdistribution/services/EmployeeService.java
+++ src/main/java/com/onedayoffer/taskdistribution/services/EmployeeService.java
@@ -5,16 +5,23 @@ import com.onedayoffer.taskdistribution.DTO.TaskDTO;
 import com.onedayoffer.taskdistribution.DTO.TaskStatus;
 import com.onedayoffer.taskdistribution.repositories.EmployeeRepository;
 import com.onedayoffer.taskdistribution.repositories.TaskRepository;
+import com.onedayoffer.taskdistribution.repositories.entities.Employee;
+import com.onedayoffer.taskdistribution.repositories.entities.Task;
+import jakarta.persistence.EntityNotFoundException;
 import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
+import org.springframework.data.domain.Sort;
 import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @AllArgsConstructor
+@Slf4j
 public class EmployeeService {
 
     private final EmployeeRepository employeeRepository;
@@ -22,32 +29,53 @@ public class EmployeeService {
     private final ModelMapper modelMapper;
 
     public List<EmployeeDTO> getEmployees(@Nullable String sortDirection) {
-        throw new java.lang.UnsupportedOperationException("implement getEmployees");
-
-        // if sortDirection.isPresent() ..
-        // Sort.Direction direction = ...
-        // employees = employeeRepository.findAllAndSort(Sort.by(direction, "fio"))
-        // employees = employeeRepository.findAll()
-        // Type listType = new TypeToken<List<EmployeeDTO>>() {}.getType()
-        // List<EmployeeDTO> employeeDTOS = modelMapper.map(employees, listType)
+        if(sortDirection == null || sortDirection.isEmpty()) {
+            return employeeRepository.findAll()
+                    .stream()
+                    .map(employee -> modelMapper.map(employee, EmployeeDTO.class))
+                    .collect(Collectors.toList());
+        }
+        if(!sortDirection.equalsIgnoreCase(Sort.Direction.ASC.toString()) && !sortDirection.equalsIgnoreCase(Sort.Direction.DESC.toString())){
+            throw new IllegalArgumentException("Неверный параметр сортировки: " + sortDirection);
+        }
+        Sort sort = Sort.by(Sort.Direction.valueOf(sortDirection), "fio");
+        return employeeRepository.findAllAndSort(sort)
+                .stream()
+                .map(employee -> modelMapper.map(employee, EmployeeDTO.class))
+                .collect(Collectors.toList());
     }
 
     @Transactional
     public EmployeeDTO getOneEmployee(Integer id) {
-        throw new java.lang.UnsupportedOperationException("implement getOneEmployee");
+        Employee employee = employeeRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Работник с id " + id + " не найден в базе данных"));    //todo в exceptHandler
+        return modelMapper.map(employee, EmployeeDTO.class);
     }
 
     public List<TaskDTO> getTasksByEmployeeId(Integer id) {
-        throw new java.lang.UnsupportedOperationException("implement getTasksByEmployeeId");
+        Employee employee = employeeRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Работник с id " + id + " не найден в базе данных"));
+        return employee.getTasks().stream().map(task -> modelMapper.map(task, TaskDTO.class)).toList();
     }
 
     @Transactional
     public void changeTaskStatus(Integer taskId, TaskStatus status) {
-        throw new java.lang.UnsupportedOperationException("implement changeTaskStatus");
+        Task task = taskRepository.findById(taskId)
+                .orElseThrow(() -> new EntityNotFoundException("Задача с id " + taskId + " не найден в базе данных"));
+        if (task.getStatus().equals(status)) {
+            log.warn("Статус задачи с id {} уже установлен как {}", taskId, status);
+            return;
+        }
+        task.setStatus(status);
+        taskRepository.save(task);
     }
 
     @Transactional
     public void postNewTask(Integer employeeId, TaskDTO newTask) {
-        throw new java.lang.UnsupportedOperationException("implement postNewTask");
+        Employee employee = employeeRepository.findById(employeeId)
+                .orElseThrow(() -> new EntityNotFoundException("Работник с id " + employeeId + " не найден в базе данных"));
+        Task task = modelMapper.map(newTask, Task.class);
+        employee.addTask(task);
+        employeeRepository.save(employee);
     }
 }
